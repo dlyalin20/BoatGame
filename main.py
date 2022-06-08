921ae7bf0beffb55d98153c409850e11dae752fc
dlyalin20 <dlyalin20@stuy.edu>
2022-06-07 23:08:48 -0400

    Update main.py
    Started inputs

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+from matplotlib import use
 import pygame as pg
 from sys import exit
-
-from pyparsing import White
 
 ####################### INITIAL SETUP #######################
 
@@ -13,6 +12,11 @@ HEIGHT = 800
 CAPTION = "Super Duper Ultra Incredible Absolutely Amazing BoatGame!"
 screen = pg.display.set_mode((WIDTH, HEIGHT))
 pg.display.set_caption(CAPTION)
+
+# colors
+WHITE = (255, 255, 255)
+WATER_BLUE = (58, 213, 199)
+BLACK = (0, 0, 0)
 
 # used for frame per second control
 clock = pg.time.Clock() 
@@ -51,6 +55,46 @@ ball = pg.transform.scale(ball, ballSize)
 
 ####################### END INITIAL SETUP #######################
 
+####################### TEXT SETUP #######################
+
+bigFont = pg.font.Font(None, 20)
+smallFont = pg.font.Font(None, 16)
+
+# control panel
+CPS = "Control Panel: "
+control_panel = bigFont.render(CPS, True, BLACK, WATER_BLUE)
+cpRect = control_panel.get_rect()
+cpRect.center = (50, 600)
+
+# Cannonball Mass
+MS = "Cannonball Mass: "
+massText = smallFont.render(MS, True, BLACK, WATER_BLUE)
+massRect = massText.get_rect()
+massRect.center = (50, 620)
+
+# Boat Mass
+BS = "Boat Mass: "
+boatText = smallFont.render(BS, True, BLACK, WATER_BLUE)
+bRect = boatText.get_rect()
+bRect.center = (35, 640)
+
+# Target Distance
+TDS = "Target Distance: "
+targetText = smallFont.render(TDS, True, BLACK, WATER_BLUE)
+tRect = targetText.get_rect()
+tRect.center = (50, 660)
+
+# Velocity Input
+velocityRect = pg.Rect(100, 100, 140, 32)
+color_inactive = pg.Color('lightskyblue3')
+color_active = pg.Color('dodgerblue2')
+color = color_inactive
+active = False
+userVelocity = ''
+done = False
+
+####################### END TEXT SETUP #######################
+
 ####################### DRIVER LOOP #######################
 
 # game driver loop (rounds loop within)
@@ -60,13 +104,43 @@ while True:
         if event.type == pg.QUIT: 
             pg.quit()
             exit()
+        # handler for clicking
+        if event.type == pg.MOUSEBUTTONDOWN:
+            # handler for clicking on velocity input
+            if velocityRect.collidepoint(event.pos):
+                active = not active
+            else:
+                active = False
+            color = color_active if active else color_inactive
+        # handler for saving user input to screen
+        if event.type == pg.KEYDOWN:
+            # handler for velocity input rendering
+            if active:
+                if event.key == pg.K_RETURN:
+                    print(userVelocity)
+                    userVelocity = ''
+                elif event.key == pg.K_BACKSPACE:
+                    userVelocity = userVelocity[:-1]
+                else:
+                    userVelocity += event.unicode
 
-    # this should be within a round
     screen.blit(sky, (0, 0))
     screen.blit(water, (0, 500))
     screen.blit(boat, (700, 440))
     screen.blit(cannon, (780, 435))
     screen.blit(sailor, (760, 460))
+    screen.blit(control_panel, cpRect)
+    screen.blit(massText, massRect)
+    screen.blit(boatText, bRect)
+    screen.blit(targetText, tRect)
+
+    # velocity input rendering --> modify location & color & size
+    velocitySurface = smallFont.render(userVelocity, True, color)
+    velocityRect.w = max(200, velocitySurface.get_width() + 10)
+    screen.blit(velocitySurface, (velocityRect.x + 5, velocityRect.y + 5))
+    pg.draw.rect(screen, color, velocityRect, 2)
+
+    # update
     pg.display.update()
     clock.tick(60)
 
